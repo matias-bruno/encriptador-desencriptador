f285c23766e830a96262908b653156bf6278f8fb
Matias Bruno <matiasbr1@hotmail.com>
2022-05-07 17:49:59 -0300

    separado algo de codigo en funciones

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
-let btnenc = document.querySelector("#btn-enc");
-btnenc.onclick = function() {
-    let input = document.querySelector("#entrada");
-    let texto = input.value;
+function encriptar(texto) {
     texto = texto.replaceAll("e","enter");
     texto = texto.replaceAll("i","imes");
     texto = texto.replaceAll("a","ai");
     texto = texto.replaceAll("o","ober");
     texto = texto.replaceAll("u","ufat");
-
+    return texto;
+}
+function desencriptar(texto) {
+    texto = texto.replaceAll("enter", "e");
+    texto = texto.replaceAll("imes", "i");
+    texto = texto.replaceAll("ai", "a");
+    texto = texto.replaceAll("ober", "o");
+    texto = texto.replaceAll("ufat", "u");
+    return texto;
+}
+let btnenc = document.querySelector("#btn-enc");
+btnenc.onclick = function() {
+    let input = document.querySelector("#entrada");
+    let texto = encriptar(input.value);
     mostrarResultado(texto);
     input.value = "";
 };
 let btndes = document.querySelector("#btn-des");
 btndes.onclick = function() {
     let input = document.querySelector("#entrada");
-    let texto = input.value;
-    texto = texto.replaceAll("enter", "e");
-    texto = texto.replaceAll("imes", "i");
-    texto = texto.replaceAll("ai", "a");
-    texto = texto.replaceAll("ober", "o");
-    texto = texto.replaceAll("ufat", "u");
-    
+    let texto = desencriptar(input.value);
     mostrarResultado(texto);
     input.value = "";
 };
